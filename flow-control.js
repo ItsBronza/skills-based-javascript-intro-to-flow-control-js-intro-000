e4255b7516c287542025ea4c67707764da039ca0
Alyona <itsbronza@yahoo.com>
2019-10-30 09:11:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,34 +1,19 @@
 function basicTeenager(age) {
-  if (age <= 19 && age => 13){
-        return You are a teenager!;
- }
+
 }
 
 function teenager(age) {
-return (13-19) ? "You are a teenager!":"You are not a teenager";
+
 }
 
 function ageChecker(age) {
-  var x;
-if(13-19){
-  return "You are a teenager!";
-} else if(x === 12 || x < 12){
-  return "You are a kid";
-} else if(x > 19){
-  return "You are a grownup";
-}
+
 }
 
 function ternaryTeenager(age) {
-return (13-19) ? "You are a teenager!":"You are not a teenager";
+
 }
 
 function switchAge(age) {
-switch(){
-case "13-19":
-return "You are a teenager!";
-break;
-default:
-return "You have an age";
-}
+
 }
